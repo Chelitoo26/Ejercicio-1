22a0e8b58319f76a96e07e17b565983c34e8e4ca
Chelitoo26 <alejocas890@gmail.com>
2023-03-22 12:39:01 -0600

    examen2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlatilloComponent } from './platillo/platillo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlatilloComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
